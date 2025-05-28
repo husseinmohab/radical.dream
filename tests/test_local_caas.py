83fbbee1a6828d1bc46abf1a71c6a13b3ece7eed
Mohab Hussein <husseinmohab05@gmail.com>
2025-05-28 13:37:55 -0400

    creating dummy params with a focus on local_caas

--- tests/test_local_caas.py
+++ tests/test_local_caas.py
@@ -2,9 +2,36 @@ from unittest import TestCase
 from hydraa.services.caas_manager.local_caas import LocalCaas 
 
 class LocalCaaSTestClass(TestCase):
-
         def test_init(self):
-                local_caas = LocalCaas()
-                self.assertIsNotNone(local_caas)
+                sandbox = 'test_sandbox'
+                manager_id = 'mgr123'
+                cred = {'auth_url': 'http://fake', 'username': 'user', 'password': 'pass'}
+                VMS = [VM()]
+                asynchronous = False
+                auto_terminate = False
+                log = Logger()
+                prof = Profiler
+                obj = LocalCaas(sandbox, manager_id, cred, VMS, asynchronous, auto_terminate, log, prof)
+                self.assertIsNotNone(obj)
 
+class VM:
+        def __init__(self):
+                self.LaunchType = 'testlaunch'
+                self.MinCount = 1
+                self.MaxCount = 1
+                self.KeyPair = None
+                self.FlavorId = 'flavor1'
+                self.ImageId = 'image1'
+                self.Network = None
+                self.UserData = ''
+
+class Logger:
+        def trace(self, msg):
+                pass
+
+class Profiler:
+        def __init__(self, name, path):
+                pass
+        def prof(self, *args, **kwargs):
+                pass
 

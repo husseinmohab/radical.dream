fbf7291dc5d57a4e24b400728673e185749372f5
Mohab Hussein <husseinmohab05@gmail.com>
2025-05-28 01:19:26 -0400

    initial creation of unit test files

---
+++ tests/test_local_caas.py
@@ -0,0 +1,13 @@
+
+
+
+
+from unittest import TestCase
+from hydraa.services.caas_manager.local_caas import LocalCaaS 
+
+class LocalCaaSTestClass(TestCase):
+
+        def test_init(self):
+                LocalCaaS()
+
+
---
+++ tests/test_manager.py
@@ -0,0 +1,8 @@
+from unittest import TestCase
+from hydraa.services.caas_manager.manager import CaaSManager
+
+class CaaSManagerTestClass(TestCase):
+	
+	def test_init(self):
+		CaaSManager()
+

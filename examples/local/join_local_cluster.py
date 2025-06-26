a83ef11ea58754d4d9099ed89f48c646f9ebb915
Mohab Hussein <husseinmohab05@gmail.com>
2025-06-26 10:43:21 -0400

    remove kube config path

--- examples/local/join_local_cluster.py
+++ examples/local/join_local_cluster.py
@@ -6,7 +6,7 @@ provider_mgr = proxy([LOCAL])
 
 # Joining a `microk8s`` requires to set `KubeConfigPath`` in the LocalVM.
 # For other clusters Hydraa should be able to pick it up automatically.
-vm = LocalVM(launch_type='join', KubeConfigPath='/Users/monoble/.kube/config')
+vm = LocalVM(launch_type='join')
 
 caas_mgr = CaasManager(provider_mgr, [vm], asynchronous=False, auto_terminate=True)
 
